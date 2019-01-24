933baedefdb8192e14de958a722b70c76febe410
mauendara <mauendara@gmail.com>
2019-01-24 00:59:41 -0500

    - Se crean dos consultas rest para la lista de poles, y el conteo de poles

--- app.py
+++ app.py
@@ -1,21 +1,46 @@
 from ivoox_data_manager import *
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+from config import *
+import logging
 
-class Episode(Resource):
+
+class PolesList(Resource):
 
     def __init__(self):
-        self.data_manager = IvooxDataManager()
+        self.config = read_config()
+        self.data_manager = IvooxDataManager(config['database_env'])
 
     def get(self):
-        return self.data_manager.get_episodes(), 200
+        poles_list = self.data_manager.get_poles_by_season(self.config['ivoox_podcast_season'])
+        poles = []
+        for p in poles_list:
+            poles.append(p.to_dict())
+        return poles, 200
+
+
+class PolesCount(Resource):
+
+    def __init__(self):
+        self.config = read_config()
+        self.data_manager = IvooxDataManager(config['database_env'])
+
+    def get(self):
+        return self.data_manager.count_poles_by_season(self.config['ivoox_podcast_season']), 200
 
 
 if __name__ == "__main__":
+    logging.basicConfig(filename='app.log',
+                        level=logging.INFO,
+                        format='%(asctime)s %(levelname)-8s %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+    config = read_config()
+
     app = Flask(__name__)
     api = Api(app)
 
-    api.add_resource(Episode, "/api/episodes")
+    api.add_resource(PolesList, "/api/poles/list")
+    api.add_resource(PolesCount, "/api/poles/count")
 
     app.run(debug=True)
 
